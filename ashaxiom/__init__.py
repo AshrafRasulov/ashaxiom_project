0696f20b93a4ce6061a4bd9854569385e4887050
Ashraf <94550959+AshrafRasulov@users.noreply.github.com>
2026-01-12 03:49:14 +0500

    update-12-01-2026

--- ashaxiom/__init__.py
+++ ashaxiom/__init__.py
@@ -1,4 +1,12 @@
 from .core import Axiom
 from .lib.threadManager import Threader
+from .lib.pipUtil.console import Console
+from .lib.pipUtil.convertData import DataConverter
 
 thread = Threader()
+
+def Convert(data):
+    return DataConverter(data)
+
+# Теперь пользователь может писать ashaxiom.Console(data)
+__all__ = ["Axiom", "thread", "Console", "Convert"]
--- ashaxiom/core.py
+++ ashaxiom/core.py
@@ -1,3 +1,31 @@
+# from .lib.main import AxiomMain
+# from .lib.brain.AxiomIntelligence import BrainCore
+
+# class Axiom:
+#     @staticmethod
+#     def start(initial_value=0):
+#         return AxiomMain(initial_value)
+    
+#     @staticmethod
+#     def think(mode="solve"):
+#         return BrainCore(AxiomMain(), mode=mode)
+    
+#     @staticmethod
+#     def static_metod(*args):
+#         """Собирает список GraphicConfig для мульти-графика."""
+#         return list(args)
+
+#     @staticmethod
+#     def static_2D(data_list):
+#         """Рисует синхронизированный 2D график. Импорт внутри для стабильности."""
+#         from .lib.graph.hGraph import hGraph
+#         return hGraph().render_static_2D(data_list)
+
+#     @staticmethod
+#     def version():
+#         return "2.3.5 (Full Integration Edition)"
+
+
 from .lib.main import AxiomMain
 from .lib.brain.AxiomIntelligence import BrainCore
 
@@ -12,15 +40,15 @@ class Axiom:
     
     @staticmethod
     def static_metod(*args):
-        """Собирает список конфигураций линий."""
+        """Собирает список объектов для мульти-графика."""
         return list(args)
 
     @staticmethod
     def static_2D(data_list):
-        """Рисует синхронизированный график. Исправлен импорт."""
+        """Синхронизированная 2D визуализация."""
         from .lib.graph.hGraph import hGraph
         return hGraph().render_static_2D(data_list)
 
     @staticmethod
     def version():
-        return "2.3.0 (Academy Edition)"
+        return "2.3.8 (Consolidated Global Edition)"
